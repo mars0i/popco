e4afee461ba1f1bf6a6bc2cbb0517dd818805248
Kristen Hammack <spacekh@uab.edu>
2012-07-12 14:30:29 -0500

    So... This is not what I expected...

---
+++ lightbulb/analogy1simple.lisp
@@ -0,0 +1,101 @@
+; analogy1simple.lisp
+; SUPER SIMPLIFIED lightbulb/tumor problem from Holyoak/Thagard 1989
+; Author:   Kristen Hammack
+; Vers:     1.0.0 07/10/2012 kmh - initial coding
+
+; These are output to NetLogo:
+(setf *propn-category-prefixes* '("L" "T"))
+(setf *propn-category-descriptions* '("lightbulb" "tumor"))
+(setf *netlogo-output-name* "../data/analogy1SimpleSwitchedNetLogoData.txt")
+
+;; first clear everything out
+(mapcar #'clear-plists (get 'folks 'members))
+(clear-person-nets 'folks)
+(setf *the-population* 'folks)
+
+
+;; CONVENTIONS:
+;; Proposition names start with a prefix indicating domain, and are mixed case after that.
+;; prefix key:
+;;
+;;      l-  : lightbulb propositions
+;;
+;;      t-  : tumor propositions
+;;
+;; All-uppercase can be used to flag things temporarily.
+
+
+;;Lightbulb Info
+(defvar lightbulb-info
+  '(
+        (laser (obj-laser) l-Obj-Laser)
+        (bulb (obj-bulb) l-Obj-Bulb)
+        (filament (obj-filament) l-Obj-Filament)
+        (surround (obj-bulb obj-filament) l-Bulb-Surrounds-Filament)
+        (outside (obj-laser obj-bulb) l-Laser-Outside-Bulb)
+   ))
+
+
+;;Tumor Info
+(defvar tumor-info
+    '(
+        (ray-source (obj-ray) t-Obj-Ray)
+        (tissue (obj-tissue) t-Obj-Tissue)
+        (tumor (obj-tumor) t-Obj-Tumor)
+        (surround (obj-tissue obj-tumor) t-Tissue-Surrounds-Tumor)
+        (outside (obj-ray obj-tissue) t-Ray-Outside-Tissue)
+    ))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Put SIMILAR and SYMLINK-IF-UNITS calls here:
+   
+(defvar similarity 
+    '(
+        (similar 'ray-source 'laser (* .8 *ident-weight*))
+        (similar 'filament 'tumor (* .1 *ident-weight*))
+    ))
+
+; Put PRESUMED and IMPORTANT calls here:
+(defvar pragmatic-relations '())
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MODEL PERSONS
+
+; The template:
+;(make-person 'name-of-person *the-population* propns-to-be-perceived-initially
+;             `((make-struc 'target 'problem '(start (,@propns-to-subst-in-here)))
+;               (make-struc 'source 'problem '(start (,@propns-to-subst-in-here)))
+;               ,@semantic-specs-to-subst-in-here)
+;             `(@,pragmatic-relations)
+;             '()) ; put 'source or 'target in list to restrict utterances to propns in that struc
+
+; make one model person
+(make-person 'alex 'folks tumor-info
+             `((make-struc 'source 'problem '(start (,@tumor-info)))
+               (make-struc 'target 'problem '(start (,@lightbulb-info)))
+               ,@similarity)
+             `(,@pragmatic-relations)
+             '())
+
+
+;*************************
+; INITIAL SETTINGS
+(setf *max-pop-ticks* 20)
+(setf *do-converse* nil)             ; Whether to send utterances between persons
+(setf *do-update-propn-nets* t)    ; Whether to update propn constraints from propn map units
+(setf *do-report-to-netlogo* t)  ; Whether to create file for input to NetLogo 
+(setf *do-report-analogy-nets-to-guess* t)
+(setf *silent-run?* t)             ; If nil, use Thagard-style verbose reporting to console
+;*************************
+
+; TEST RUN
+(print (get 'folks 'members))
+(format t "~%Running Model Now.~%")
+
+; To run the model
+(init-pop)
+(popco1)
+(popco1)
+(write-person-nets "../data/guess/")
