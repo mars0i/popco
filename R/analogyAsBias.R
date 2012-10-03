c0bd38ebfc207c78e4d0321fb67880710c60c709
mabrams <mabrams@uab.edu>
2012-10-03 08:15:06 -0500

    Added print statements to analogyAsBias.R

--- R/analogyAsBias.R
+++ R/analogyAsBias.R
@@ -1,10 +1,10 @@
 
-setwd("parenting18analogsAsBiases1i")
-
+print("loading data into full multirun array...")
 csvs <- list.files(pattern="*.csv")
-a <- read2multirunRA(csvs)
+print(system.time(a <- read2multirunRA(csvs)))
 # This has dimensions person, proposition, tick, run.
 
+print("subsetting into P/H at 1500 arrays...")
 #ap <- multiRA2domRA(a, "P")  # and for domain P
 #ah <- multiRA2domRA(a, "H")  # extract subarray for proposition domain H
 #ap1500 <- ap[1:9,,1500,] # we also strip out SP, the true believer/perceiver, which is person 10
@@ -16,6 +16,7 @@ ah1500 <- multiRA2domRA(a, "H")[1:9,,1500,] # see preceding notes
 # Add check here that persons have settled into the same state within each run, 
 # except for SP, the true believer/perceiver.  This should be the case.
 
+print("making arrays of per person/run average activations")
 ap1500m <- apply(ap1500, c(1,3), mean) # for each person and run,
 ah1500m <- apply(ah1500, c(1,3), mean) # get average proposition activation
 # These matrices have dimensions person, run.
@@ -23,18 +24,22 @@ ah1500m <- apply(ah1500, c(1,3), mean) # get average proposition activation
 # (Or add the check here.  Within each run, each person's average should be
 # almost the same.  But the check above is more careful.)
 # Here's one way to check:
-sum(apply(ap1500m, c(2), var))
-sum(apply(ah1500m, c(2), var))
+#sum(apply(ap1500m, c(2), var))
+#sum(apply(ah1500m, c(2), var))
 # Or since the preceding is sensitive to number of persons, you could do:
-mean(apply(ap1500m, c(2), var))
-mean(apply(ah1500m, c(2), var))
+
+print("average of run variances for P:")
+print(mean(apply(ap1500m, c(2), var)))
+print("average of run variances for H:")
+print(mean(apply(ah1500m, c(2), var)))
 # If the check succeeds, giving small values, then you can just average all
 # of the persons in a run together (so maybe combine steps later):
-
+print("making per-run average vectors:")
 ap1500mm <- apply(ap1500m, c(2), mean) # Since within a run, each person as about the same average activation,
 ah1500mm <- apply(ah1500m, c(2), mean) # we might as well just use the average across all persons in each run.
 # Note these are vectors, with length equal to the number of runs.
 
+print("making 2-column matrix for plotting:")
 # now combine these into a two-column matrix for use by plot():
 both1500mm <- cbind(ah1500mm, ap1500mm)  # the first arg will be x, the second y
 
