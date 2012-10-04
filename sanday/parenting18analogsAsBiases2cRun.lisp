68bbbf35ceef630108c46c12054208b537daa7fb
mabrams <mabrams@uab.edu>
2012-10-04 07:54:32 -0500

    New Lisp scripts for analogs as biases.

---
+++ sanday/parenting18analogsAsBiases2cRun.lisp
@@ -0,0 +1,4 @@
+(unlock-package 'common-lisp)
+(defvar *data-dir* "../data/parenting18analogsAsBiases2c") ; second call to defvar *data-dir* in popco.lisp will be ignored
+(load "start")
+(load "sanday/parenting18analogsAsBiases2c")
