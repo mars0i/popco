b33d731aa9fcf98881375ff59429482a08bdd112
mabrams <mabrams@uab.edu>
2012-10-06 13:25:19 -0500

    bug fix: negation must be after init-pop

--- sanday/parenting18analogsAsBiases2d.lisp
+++ sanday/parenting18analogsAsBiases2d.lisp
@@ -10,12 +10,16 @@
 (setf *do-converse* t)
 
 (make-all-origin-lifestyle-talker 'aa lifestyle-propns)  ; "aa" for "assured advocate" [propns are alpha-sorted, so insures this person is 1st]
-(mapc #'negate-propn (get 'aa-env 'all-propositions))    ; crude low-level method to cause aa to *disbelieve* all lifestyle propositions
 (make-all-origin-lifestyle-talker 'temp-person)
 (n-persons-with-name 'temp-person 'p (1- *my-pop-size*)) ; "p" for person
 (rem-elt-from-property 'temp-person 'folks 'members)
 
 (init-pop)
+
+; AT THIS POINT, THE PROPOSITIONS IN AA'S ENV HAVE BEEN INITIALIZED WITH ACTIVNS = 1.0
+; NOW NEGATE THEM:
+(mapc #'negate-propn (get 'aa-env 'all-propositions))    ; crude low-level method to cause aa to *disbelieve* all lifestyle propositions
+
 (print (get 'folks 'members))
 
 (setf *max-pop-ticks* 1500)
