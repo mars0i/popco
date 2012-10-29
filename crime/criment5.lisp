0b5368d8f916b9c076812d9af82af0aaae675116
mars0i <mabrams@uab.edu>
2012-10-29 07:33:30 -0500

    new revisions for beast analogy

---
+++ crime/criment5.lisp
@@ -0,0 +1,269 @@
+
+; Notational conventions:
+; v-: virus
+; c-: crime
+; cv-: virus-ey crime
+; cb-: beast-ey crime
+; b-: beast
+; X->Y: X causes Y to occur, where X and Y are propositions
+; X->-Y: X prevents Y from occuring, where X and Y are propositions
+
+(setf *propn-category-prefixes* '("V" "B" "CV" "CB"))
+(setf *propn-category-descriptions* '("virus propns" "beast propns" "virus-like crime propns" "beast-like crime propns"))
+
+; Question: How *is* crime like a virus?  What are the analogs of
+; infection, people, cells, etc.  In the case of viruses, the virus is the
+; essentially harming element, and it is transmitted from the harmed to
+; the not yet harmed.  In the case of crime, being a criminal is
+; transmitted, but crime harms the non-criminals more than criminals
+; [according to common ways of thinking, at least, and contrary to Socrates' view].
+
+; On the other hand, it's not clear that the intuition that criminality spreads
+; from one individual to another is that clear.  I guess it is reflected in some
+; of the preventative measures that people entertain.  But I feel that a lot
+; of virusey thinking about crime is vague.
+
+; The main difference between disease and criminality is that although
+; criminality spreads, harming the new criminals, its main harmful effects
+; are on non-criminals.  Disease can have a similar effect, in that some
+; of those infected might not pass it on; they are like innocent vicitms
+; of crime.  But that's less common and less central than in the crime
+; case, and the negative effects fall heavier on crime victims than
+; criminals, whereas typically non-transmitted infections are not worse
+; than the transmitted ones (although variation in virulence can create
+; this effect sometimes).  Note that vampirism is like a virus with the
+; side-effects on innocents of crime.  Drug addiction and sometimes
+; alcoholism are similar, sometimes.
+
+; Also note that imprisoning criminals does not get rid of all crime,
+; since at the very least the crime for which the criminal was
+; convicted occured.  The same is true of reform of criminals.
+; (On the other hand, imprisonment serves not only to take the criminal
+; off the street, but also to deter other crimes.)
+
+; Whereas support to prevent individuals from committing crimes could
+; in principle get rid of all crime, just as innoculation and other
+; preventative measures could in principle get rid of all infections.
+
+; Note that there's also a well-known effect of imprisoning causing
+; more criminality after a prisoner is released.
+
+; NOTE THESE ARE SIMPLY BASED ON MY INTUITIONS (philosophy/linguistics style)
+; AND REALLY OUGHT TO COME FROM DATA.
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar general-semantic-relations
+  '(
+    (similar 'cause 'prevent (* -1 *ident-weight*)) ; avoid mapping cause to prevent
+   ))
+
+; Note we call the things that get infected "elts", i.e. elements,
+; since they could either be people or cells or something like that.
+; Also see note about naming person objects the same elsewhere in this file.
+
+(defvar virus-propns
+  '(
+    (is-infected (prev-infected-elt) v-ip) ; the existence of a previously infected element is assumed
+    (not-infected (at-risk-elt) v-na)  ; at risk element is not infected
+    (is-infected (at-risk-elt) v-ia)   ; at-risk person/thing is infected
+
+    (harmed (at-risk-elt) v-ha)        ; note we have no quantificatiion or true pattern matching
+    (cause (v-ia v-ha) v-ciha)
+
+    (harmed (prev-infected-elt) v-hp)  ; drop? introduces noise?
+    (cause (v-ip v-hp) v-cihp)         ; drop? introduces noise?
+
+    (infect (prev-infected-elt at-risk-elt) v-ipa) ; infection spreads from the previously infected to the at-risk
+    (cause (v-ipa v-ia) v-ipa->ia) ; transmission from infected to uninfected causes infection
+
+    ; The following triplets are a bit awkward and convoluted because we don't have time indexing:
+
+    (innoculate (at-risk-elt) v-ica)   ; innoculating the at-risk prevents spread to new individuals (or something with cells?)
+    (prevent (v-ica v-ipa) v-ia->-spa) ; innoculation of uninfected prevents further infection
+    (cause (v-ia->-spa v-na) v-iaspa->na) ; preventing spread of infection causes [preserves] lack of infection in the at-risk
+
+    ; Next two triplets are structurally identical. Maybe drop one.
+
+    (quarantine (prev-infected-elt) v-qp) ; previously infected is quarantined (or cells sequestered, I suppose)
+    (prevent (v-qp v-ipa) v-qp->-spa) ; quarantining the infected prevents spread to new individuals 
+    (cause (v-qp->-spa  v-na) v-qpspa->na)
+
+    (treat (prev-infected-elt) v-tp)  ; treatment of the infected to remove disease prevents later spread
+    (prevent (v-tp v-ipa) v-tp->-spa) ; treatment of infected prevents further infection
+    (cause (v-tp->-spa v-na) v-tpspa->na)
+    ; [We elide the step in which the infected becomes uninfected, which would require time-indexing.]
+   ))
+
+(defvar virus-semantic-relations
+  '(
+    (similar 'is-infected 'not-infected (* -1 *ident-weight*))
+    ; do we need this next one given preceding?:
+    ;(semantic-iff 'v-ia 'v-na -1.0) ; at-risk-pers being infected and being uninfected are inconsistent [-1 too strong?]
+   ))
+
+
+(defvar crime-propns
+  '(
+    (is-criminal (prev-criminal-pers) cv-cp) ; person who's already committing crimes
+    (not-criminal (at-risk-pers) cv-na)      ; person at risk of turning to crime
+    (is-criminal (at-risk-pers) cv-ca)
+
+    (harmed (at-risk-pers) cv-ha)
+    (cause (cv-ca cv-ha) cv-ca->hp)         ; becoming a criminal has bad consequences for the individual
+
+    (harmed (prev-criminal-pers) cv-hp)
+    (cause (cv-cp cv-hp) cv-cp->hp)         ; being a criminal has bad consequences for the criminal
+
+    (recruit (prev-criminal-pers at-risk-pers) cv-rpa) ; criminals recruit, teach, are role models for, cause indirectly new criminals
+    (cause (cv-rpa cv-ca) cv-sca->ca) ; 
+
+    ; The following triplets are a bit awkward and convoluted because we don't have time indexing:
+
+    (support (at-risk-elt) cv-sa) ; support = financial support, supportive parents, mentors, etc., education, etc.
+    (prevent (cv-sa cv-rpa) cv-sa->-rpa)
+    (cause (cv-sa->-rpa cv-na) cv-sarpa->na)
+
+    ; Next two triplets are structurally identical. Maybe drop one.
+
+    (imprison (prev-criminal-pers) cv-ip)
+    (prevent (cv-ip cv-rpa) cv-ip->-rpa)
+    (cause (cv-ip->-rpa  cv-na) cv-iprpa->na) ; imprisoning prevents crime
+
+    (reform (prev-criminal-pers) cv-rp) ; reform includes social support, education, etc. to criminals
+    (prevent (cv-rp cv-rpa) cv-rp->-rpa)
+    (cause (cv-rp->-rpa cv-na) cv-rprpa->na)
+
+    ; In addition to criminals being harmed by being criminals,
+    ; non-criminals are harmed by criminals [not analogous to virus]:
+
+    (not-criminal (no-risk-pers) c-nn)   ; person not at risk of turning to crime
+    (victimize (prev-criminal-pers no-risk-pers) cv-vpn)
+    (harmed (no-risk-pers) c-hn)
+    (cause (cv-vpn cv-hn) c-vpn->hn) ; existing criminals harm those not at risk
+    ;; NEED SIMILAR PROPNS FOR AT-RISK, BUT ONLY IF CRIMINALIZED
+    ; old versions:
+    ;(cause (cv-cp cv-hn) c-cp->hn) ; existing criminals harm those not at risk
+    ;;(cause (cv-ca cv-hn) c-ca->hn) ; at-risk persons do too, if they turn to crime
+
+    ; TODO:
+    ; THEN DO BEASTLY-CRIME
+
+   ))
+
+(defvar crime-semantic-relations
+  '(
+    (similar 'is-criminal 'not-criminal (* -1 *ident-weight*))
+    ; do we need this next one given preceding?:
+    ;(semantic-iff 'cv-ca 'cv-na -1.0) ; at-risk-pers being infected and being uninfected are inconsistent [-1 too strong?]
+   ))
+
+
+; Note:
+; It's desirable (required?) to give different names to person objects
+; here and in crime-propns.  Even though they're persons, they're not
+; really the same persons playing a role in a crime scenario and in 
+; a beast scenario.  They can/should get identified by the analogizing
+; process, but the process can do that.
+; So rather than calling what's attacked an at-risk-pers (like people
+; who might become criminals) or a no-risk-pers (like those who are
+; harmed by crime), we use another term for potential beast victims.
+
+(defvar beast-propns
+  '(
+    (beastly (beast) b-bb)     ; we need a beast property to go with the object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    (human (person) b-pp)
+    (attacks (beast person) b-abp) ; IF THIS SHOULDN'T IT PARALLEL CRIME: victimize
+    (harmed (person) b-hp)
+    (cause (b-abp b-hp) b-abp->hp) ; being attacked is harmful
+    ; simple version:
+    ;(cause (b-bb b-hp) b-bb->hp) ; beasts harm persons
+
+    (kill (person beast) b-kpb)
+    (prevent (b-kpb b-bb->hp) b-kpb->-bb->hp)
+    (capture (person beast) b-cpb)
+
+    ; The following are borrowed from the Sanday simulations:
+    ;(hunts-endangers (people beast) b-Person-Endangers-Beast)
+    ;(harms (beast people) b-Beast-Harms-Person)
+    ;(causes (b-Person-Endangers-Beast b-Beast-Harms-Person) b-Hunting-Is-Dangerous)
+    ;(distant-agent (beast people) b-Beast-Distant)
+   ))
+
+
+(defvar beast-semantic-relations
+  '(
+    (similar 'beastly 'human (* -.5 *ident-weight*))
+    ; do we need this next one given preceding?:
+    ;(semantic-iff 'cv-ca 'cv-na -1.0) ; at-risk-pers being infected and being uninfected are inconsistent [-1 too strong?]
+   ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Note that I'm reversing source vs target wrt the sanday simulations:
+
+(defun make-test-person (name &optional (given '()))
+    (make-person name 'folks given
+                 `((make-struc 'target 'problem '(start (,@crime-propns)))
+                   (make-struc 'source 'problem '(start (,@virus-propns)))
+                   ,@virus-semantic-relations)
+                 '()
+                 '(target)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; leave these at defaults:
+; *propn-excit-weight* *propn-inhib-weight* *trust* *perceived-excit*
+;(setf *time-runs* nil)
+(setf *do-converse* nil)
+(setf *do-update-propn-nets* t)
+(setf *do-report-to-netlogo* t)
+(setf *do-report-propns-to-csv* t)
+(setf *do-report-analogy-nets-to-guess* t)
+(setf *sleep-delay* nil)           ; If non-nil, pause this many seconds between generations
+(setf *silent-run?* t)             ; If nil, use Thagard-style verbose reporting to console
+
+
+;; first clear everything out
+(mapcar #'clear-plists (get 'folks 'members))
+(clear-person-nets 'folks)
+(setf *the-population* 'folks)
+
+(make-test-person 'andy crime-propns)
+
+(print (get 'folks 'members))
+
+(setf *max-pop-ticks* 200)
+(init-pop)
+
