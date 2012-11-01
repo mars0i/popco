f5162555b544f94137116cda984f7a4cc7487258
mars0i <mabrams@uab.edu>
2012-11-01 08:45:15 -0500

    in process of debugging the basic check model

--- crime/criment6basicCheck1.lisp
+++ crime/criment6basicCheck1.lisp
@@ -7,18 +7,32 @@
 (setf *extra-meta-commands* "")
 (setf *do-converse* NIL)
 
+SOMETHING IS WRONG WITH THE VIRUS-PROPNS (ONLY THEM)
+SOMETHING IS WRONG WITH THE VIRUS-PROPNS (ONLY THEM)
+SOMETHING IS WRONG WITH THE VIRUS-PROPNS (ONLY THEM)
+
 ; *do-converse* is set off, so the fact that these persons are
 ; allowed to talk about crime will have no effect:
+(make-both-bias-crime-talker 'Vicky viral-crime-propns)
+(make-both-bias-crime-talker 'Becky beastly-crime-propns)
+(make-both-bias-crime-talker 'Bea beast-propns)
+(make-both-bias-crime-talker 'Virgil virus-propns)
 
-(make-both-bias-crime-talker 
+(make-both-bias-crime-talker 'nay-Vicky viral-crime-propns)
+(make-both-bias-crime-talker 'nay-Becky beastly-crime-propns)
+(make-both-bias-crime-talker 'nay-Bea beast-propns)
+(make-both-bias-crime-talker 'nay-Virgil virus-propns)
 
-;(make-both-bias-crime-talker 'aa crime-propns)  ; "aa" for "assured advocate" [propns are alpha-sorted, so insures this person is 1st]
-;(make-both-bias-crime-talker 'temp-person)
-;(n-persons-with-name 'temp-person 'p (1- *my-pop-size*)) ; "p" for person
-;(rem-elt-from-property 'temp-person 'folks 'members)
-
-(init-pop)
 (print (get 'folks 'members))
 
-(setf *max-pop-ticks* 1000)
-(popco)
+;(init-pop)
+
+; crude low-level method to cause to *disbelieve* all formerly perceived propositions.
+; note this has to happen after init-pop.
+(mapc #'negate-propn (get 'nay-Vicky 'all-propositions))
+(mapc #'negate-propn (get 'nay-Becky 'all-propositions))
+(mapc #'negate-propn (get 'nay-Bea 'all-propositions))
+(mapc #'negate-propn (get 'nay-Virgil 'all-propositions))
+
+(setf *max-pop-ticks* 50)
+;(popco)
