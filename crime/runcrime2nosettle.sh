e99bf53a1aef36f8f9fbb81e13828f1649e8bfb7
mabrams <mabrams@uab.edu>
2012-12-27 08:29:26 -0600

    added some multi-run scripts

---
+++ crime/runcrime2nosettle.sh
@@ -0,0 +1,5 @@
+date
+run=75; time while [ $run -gt 0 ] ; do sbcl --eval '(progn (defvar *data-dir* "../data/lateDecSeproadback/crime2b/nosettle") (defvar *min-pop-ticks-to-settle* nil))' --load start --load c/crime2b ; run=$((run-1)); done
+run=75; time while [ $run -gt 0 ] ; do sbcl --eval '(progn (defvar *data-dir* "../data/lateDecSeproadback/crime2c/nosettle") (defvar *min-pop-ticks-to-settle* nil))' --load start --load c/crime2c ; run=$((run-1)); done
+run=75; time while [ $run -gt 0 ] ; do sbcl --eval '(progn (defvar *data-dir* "../data/lateDecSeproadback/crime2d/nosettle") (defvar *min-pop-ticks-to-settle* nil))' --load start --load c/crime2d ; run=$((run-1)); done
+date
