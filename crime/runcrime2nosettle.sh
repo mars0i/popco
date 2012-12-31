d78141ab4b5733136a385d9a39b9010084e54c75
mabrams <mabrams@uab.edu>
2012-12-31 11:56:54 -0600

    Forgot to commit last week, initial full version of these files, I think.

--- crime/runcrime2nosettle.sh
+++ crime/runcrime2nosettle.sh
@@ -1,5 +1,12 @@
+#!/bin/sh
 date
+git branch
+echo from github repository
+echo settling prevented
 run=75; time while [ $run -gt 0 ] ; do sbcl --eval '(progn (defvar *data-dir* "../data/lateDecSeproadback/crime2b/nosettle") (defvar *min-pop-ticks-to-settle* nil))' --load start --load c/crime2b ; run=$((run-1)); done
 run=75; time while [ $run -gt 0 ] ; do sbcl --eval '(progn (defvar *data-dir* "../data/lateDecSeproadback/crime2c/nosettle") (defvar *min-pop-ticks-to-settle* nil))' --load start --load c/crime2c ; run=$((run-1)); done
 run=75; time while [ $run -gt 0 ] ; do sbcl --eval '(progn (defvar *data-dir* "../data/lateDecSeproadback/crime2d/nosettle") (defvar *min-pop-ticks-to-settle* nil))' --load start --load c/crime2d ; run=$((run-1)); done
 date
+git branch
+echo from github repository
+echo settling prevented
--- crime/runcrime2settle.sh
+++ crime/runcrime2settle.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
+date
+git branch
+echo from github repository
+echo default 5 pop-tick settling
 run=75; time while [ $run -gt 0 ] ; do sbcl --eval '(defvar *data-dir* "../data/lateDecSeproadback/crime2b/settle")' --load start --load c/crime2b ; run=$((run-1)); done
 run=75; time while [ $run -gt 0 ] ; do sbcl --eval '(defvar *data-dir* "../data/lateDecSeproadback/crime2c/settle")' --load start --load c/crime2c ; run=$((run-1)); done
 run=75; time while [ $run -gt 0 ] ; do sbcl --eval '(defvar *data-dir* "../data/lateDecSeproadback/crime2d/settle")' --load start --load c/crime2d ; run=$((run-1)); done
+date
+git branch
+echo from github repository
+echo default 5 pop-tick settling
