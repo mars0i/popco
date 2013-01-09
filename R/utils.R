7f9412a861a518be580bf3e418384353dec162cf
mars0i <mabrams@uab.edu>
2013-01-09 11:02:12 -0600

    added convenient alternative to R's ls().

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,8 @@
 # utils.R
 
-maxabs <- function(x) { max(abs(x)) }
+maxabs <- function(x){ max(abs(x)) }
 
-maxabsdiff <- function(x, y) { maxabs(x - y) }
+maxabsdiff <- function(x, y){ maxabs(x - y) }
+
+# like ls(), but more convenient
+mls <- function(pat){ ls(globalenv(), pattern=pat) }
