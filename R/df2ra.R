ede37753cac36b1744d01c3bccf3ae71b59bf0b4
mabrams <mabrams@uab.edu>
2013-01-13 20:02:21 -0600

    added comments

--- R/df2ra.R
+++ R/df2ra.R
@@ -327,6 +327,7 @@ multiRA2meanDF <- function(multiRA, dom1, dom2, lastTick=dim(multiRA)[3], firstT
   df
 }
 
+# dfs and biases can be either lists or vectors, I believe
 combineMeanDFsWithBiases <- function(dfs=NULL, biases=NULL) {
   if (length(dfs) == 0 || length(biases) == 0) {stop("dfs or biases is empty.")}
   if (length(dfs) != length(biases)) {stop("lengths of dfs and biases are not the same.")}
@@ -334,7 +335,7 @@ combineMeanDFsWithBiases <- function(dfs=NULL, biases=NULL) {
   df <- NULL
 
   for (i in 1:length(dfs)){
-    dfs[[i]]$bias <- biases[[i]]
+    dfs[[i]]$bias <- biases[[i]] # double brackets works with both lists and vectors; single brackets only for vectors here
   }
 
   do.call(rbind, dfs)
