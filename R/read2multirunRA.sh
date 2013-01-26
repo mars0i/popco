8ae102dd45e876821a74ec68cfcd9062cfa1f93b
Marshall Abrams <mabrams@uab.edu>
2013-01-26 00:33:30 -0600

    added path argument for loc of csvs to read2multirunRA.sh

--- R/read2multirunRA.sh
+++ R/read2multirunRA.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-if [ -z "$2" ]; then
-  echo usage: $0 arrayame vector-of-csv-names
+if [ -z "$3" ]; then
+  echo usage: $0 datadir arrayame vector-of-csv-names
+  echo datadir is a relative path wrt the default data dir--probably ../data wrt the popco dir
   echo arrayname will also be used as filename with .rdata added
   echo vector-of-csv-names should have no spaces
   exit 1
 fi
 
-arrayname="$1"
-csvvec="$2"
+datadir="$1"
+arrayname="$2"
+csvvec="$3"
 filename="${arrayname}.rdata"
 
 R --no-save << END
 source("~/pop/R/df2ra.R")
+setwd("$datadir")
 $arrayname <- read2multirunRA($csvvec)
 save($arrayname, file="$filename")
 END
