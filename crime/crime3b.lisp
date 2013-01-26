19a5b3024906ccbfa1aa7df2125ffdd96c53629b
mars0i <mabrams@uab.edu>
2013-01-25 23:11:42 -0600

    new test using virus bias with crime3.lisp

---
+++ crime/crime3b.lisp
@@ -0,0 +1,25 @@
+;; crime3b.lisp
+
+(load "nosettle")      ; don't allow networks to settle, ever, since subtle differences are amplified by communication
+(load "crime/crime3")
+
+(setf *do-report-to-netlogo* nil)
+
+(defvar *my-pop-size* 20)
+
+; don't move graph around in telguess:
+(setf *guess-layout-commands* "")
+(setf *extra-meta-commands* "")
+(setf *do-converse* t)
+
+(make-virus-bias-crime-talker 'aa crime-propns)  ; "aa" for "assured advocate" [propns are alpha-sorted, so insures this person is 1st]
+(make-virus-bias-crime-talker 'temp-person)
+(n-persons-with-name 'temp-person 'p (1- *my-pop-size*)) ; "p" for person
+(rem-elt-from-property 'temp-person 'folks 'members)
+
+(init-pop)
+(print (get 'folks 'members))
+
+(setf *max-pop-ticks* 5000)
+(popco)
+(quit)
