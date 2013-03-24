d11bc78e13c4ef0b51876423b23908d7f4d657e2
mars0i <mabrams@uab.edu>
2013-03-24 02:29:30 -0500

    added info/functions for multiple-bias, multiple-domain ks.boot comparisons.

--- R/popcotests.R
+++ R/popcotests.R
@@ -3,6 +3,22 @@
 # see also:
 # findRunsWithDisagreement in df2ra.R
 
+# here's a way to run ks.boot on several combinations:
+#for (bs in c("virus","virus-talk","beast","beast-talk")){
+#  for (dom in c("CV","CB")){
+#    cat(bs,dom,"pvalue =",ks.boot(df[df$bias==bs,dom],dfa[dfa$bias==bs,dom],nboots=1000)$ks.boot.pvalue,"\n")
+#  }
+#}
+
+# run multiple ks.boot comparions for all combinations of a set of biases and a set of domains:
+ksBootAllBiasesAllDoms <- function(df1, df2, biases, doms) {
+  for (bs in biases){
+    for (dom in doms){
+      cat(bs,dom,"pvalue =",ks.boot(df1[df1$bias==bs,dom],df2[df2$bias==bs,dom],nboots=1000)$ks.boot.pvalue,"\n")
+    }
+  }
+}
+
 # Given two mean dfs produced by multiRA2meanDF, do a Kolmogorov-Smirnov 
 # test with bootstrapped significance for sameness of distributions 
 # for a given domain.  Uses Matching::ks.boot.
