c8ff81d606f75f32bfc6c2e4952b88b81ad45c0f
mabrams <mabrams@uab.edu>
2013-05-30 21:01:25 -0500

    comments, mv's.

--- R/df2ra.R
+++ R/df2ra.R
@@ -483,12 +483,12 @@ combineMeanDFsWithBiases <- function(dfs=NULL, biases=NULL) {
   do.call(rbind, dfs)
 }
 
-combineMeanDFsWithBiases.old <- function(df1, bias1, df2, bias2) {
-  # note these are changes to the internal copies, not the function arguments:
-  df1$bias <- bias1
-  df2$bias <- bias2
-  rbind(df1, df2)
-}
+#combineMeanDFsWithBiases.old <- function(df1, bias1, df2, bias2) {
+#  # note these are changes to the internal copies, not the function arguments:
+#  df1$bias <- bias1
+#  df2$bias <- bias2
+#  rbind(df1, df2)
+#}
 
 ##############################################################
 # NOT CURRENTLY USED:
