67c370127c3cb0b7cdcd1698b91e322cb34003bc
mabrams <mabrams@uab.edu>
2013-06-23 11:50:29 -0500

    partway through thorough upgrade of comments on crime3 propositions

--- crime/crime3propns.lisp
+++ crime/crime3propns.lisp
@@ -1,20 +1,24 @@
 ;;; crime3propns.lisp
 ;;; The crime3 version of the "crime is a virus/beast" propositions.
-;;; To be included by crime3.lisp.
-;;; This file has the propositonal and semantic contents of
-;;; crime3.lisp as of 2/20/2013, with everything but the
-;;; propositions and semantic specs stripped out.
-;;; For possible embedding in presentations and papers.
-;;; Comments add/altered after 6/22/2013.
-;;; See crime3withnotes.lisp for all of the original comments,
-;;; possible propositions not in use, etc.
+;;; To be loaded by crime3.lisp.
+;;; This file has all of the the propositonal and semantic contents of
+;;; crime3.lisp as of 2/20/2013, with everything but the propositions and 
+;;; semantic specs stripped out.  In addition to things like person-defining
+;;; functions, I stripped out candidate propositions that I'd commented out
+;;; and have not used for a long time.
+;;; See crime3withnotes.lisp for all of the original comments, possible propositions not in use, etc.
 
 ; Notational conventions:
-; v-: virus | c-: crime | cv-: virus-ey crime | cb-: beast-ey crime | b-: beast
+; Source analog: V-: virus propn | B-: beast propn 
+; Target analog: CV-: virus-ey crime propn | CB-: beast-ey crime propn
+; BPERSON: person in the beast/virus domains; CPERSON: person in the crime domains
+; ELTS: things subject to viruses or crime in the virusey sense 
 ; X->Y: X causes Y to occur, where X and Y are propositions
 ; X->-Y: X prevents Y from occuring, where X and Y are propositions
-; bperson: person in the beast/virus domain; ; cperson: person in the crime domain
-; elts: things subject to viruses or crime in the virusey sense 
+; HO1: This is a higher-order proposition referencing only first-order propositions
+; HO2: This is a higher-order proposition referencing at least one HO1 proposition
+; HO3: This is a higher-order proposition referencing at least one HO2 proposition
+; etc.
 
 (defvar virus-propns
   '(
@@ -33,53 +37,53 @@
     (cause (v-qp->-spa  v-na) v-qpspa->na)
    ))
 
-(defvar beast-propns
-  '(
-    (human (bperson) b-pp)   ; supposed to match: (not-criminal (cperson) cb-np)
-    (aggressive (beast) b-ab)
-    (attack (beast bperson) b-abp)
-    (cause (b-ab b-abp) b-ab->abp)
-    (harms (bperson) b-hp)
-    (cause (b-abp b-hp) b-abp->hp) ; being attacked is harmful
-    (helps (beast) b-hb)
-    (cause (b-abp b-hb) b-abp->hb) ; beasts benefit from attacking--e.g get food
-    (capture (bperson beast) b-cpb)
-    (prevent (b-cpb b-abp) b-cpb->-abp)
-    (danger-to (bperson) b-dtp)
-    (cause (b-cpb b-dtp) b-cpb->dtp)
-   ))
-
 (defvar viral-crime-propns
   '(
-    (is-criminal (prev-criminal-cperson) cv-cp) ; person who's already committing crimes
-    (not-criminal (at-risk-cperson) cv-na)      ; person at risk of turning to crime
-    (is-criminal (at-risk-cperson) cv-ca)
-    (harms (at-risk-cperson) cv-ha)
+    (is-criminal (crim-pers) cv-cp) ; person who's already committing crimes
+    (not-criminal (at-risk-cpers) cv-na)      ; person at risk of turning to crime
+    (is-criminal (at-risk-cpers) cv-ca)
+    (harms (at-risk-cpers) cv-ha)
     (cause (cv-ca cv-ha) cv-ca->hp)         ; becoming a criminal has bad consequences for the individual
-    (recruit (prev-criminal-cperson at-risk-cperson) cv-rpa) ; criminals recruit, teach, are role models for, cause indirectly new criminals
+    (recruit (crim-pers at-risk-cpers) cv-rpa) ; criminals recruit, teach, are role models for, cause indirectly new criminals
     (cause (cv-rpa cv-ca) cv-sca->ca) ; 
-    (support (at-risk-cperson) cv-sa) ; support = financial support, supportive parents, mentors, etc., education, etc.
+    (support (at-risk-cpers) cv-sa) ; support = financial support, supportive parents, mentors, etc., education, etc.
     (prevent (cv-sa cv-rpa) cv-sa->-rpa)
     (cause (cv-sa->-rpa cv-na) cv-sarpa->na)
-    (imprison (prev-criminal-cperson) cv-ip)
+    (imprison (crim-pers) cv-ip)
     (prevent (cv-ip cv-rpa) cv-ip->-rpa)
     (cause (cv-ip->-rpa  cv-na) cv-iprpa->na) ; imprisoning prevents crime
    ))
 
+(defvar beast-propns
+  '(
+    (human (bpers) b-pp)                  ; person is human [should match: (not-criminal (cpers) cb-np)]
+    (aggressive (beast) b-ab)             ; beast is agressive
+    (attack (beast bpers) b-abp)          ; beast attacks person
+    (cause (b-ab b-abp) b-ab->abp)        ; beast's agressiveness causes it to attack person [HO1]
+    (harms (bpers) b-hp)                  ; person is harmed
+    (cause (b-abp b-hp) b-abp->hp)        ; beast attacking human harms person [HO1]
+    (helps (beast) b-hb)                  ; beast is benefited
+    (cause (b-abp b-hb) b-abp->hb)        ; beast attacking person benefits beast [HO1]
+    (capture (bpers beast) b-cpb)         ; person captures beast
+    (prevent (b-cpb b-abp) b-cpb->-abp)   ; person capturing beast prevents beast attacking person [HO1]
+    (danger-to (bpers) b-dtp)             ; person is subject to danger
+    (cause (b-cpb b-dtp) b-cpb->dtp)      ; person capturing beast is dangerous to person [HO1]
+   ))
+
 (defvar beastly-crime-propns
   '(
-    (not-criminal (cperson) cb-np)   ; person not at risk of turning to crime
-    (aggressive (prev-criminal-cperson) cb-ap)
-    (victimize (prev-criminal-cperson cperson) cb-vpp)
-    (cause (cb-ap cb-vpp) cb-ap->vpp)
-    (harms (cperson) cb-hcp) ; hp already in use as a name
-    (cause (cb-vpp cb-hcp) cb-vpp->hcp) ; existing criminals harm those not at risk
-    (helps (prev-criminal-cperson) cb-hp)
-    (cause (cb-vpp cb-hp) cb-vpp->hp) ; criminals benefit from attacking--e.g get money
-    (capture (cperson prev-criminal-cperson) cb-cpc) ; cp is already used as name for crime propn
-    (prevent (cb-cpc cb-vpp) cb-cpc->-vpp)
-    (danger-to (cperson) cb-dtp)
-    (cause (cb-cpc cb-dtp) cb-cpc->dtp)
+    (not-criminal (cpers) cb-np)           ; cpers is not a crinimal
+    (aggressive (crim-pers) cb-ap)         ; person who's already a criminal is aggressive
+    (victimize (crim-pers cpers) cb-vpp)   ; criminal victimizes non-criminal
+    (cause (cb-ap cb-vpp) cb-ap->vpp)      ; criminal's aggressiveness causes himer to victimize non-criminal [HO1]
+    (harms (cpers) cb-hcp)                 ; non-criminal is harmed [hp already in use as name]
+    (cause (cb-vpp cb-hcp) cb-vpp->hcp)    ; criminal victimizing non-criminal harms non-criminal [HO1]
+    (helps (crim-pers) cb-hp)              ; criminal is benefited
+    (cause (cb-vpp cb-hp) cb-vpp->hp)      ; criminal victimizing non-criminal benefits criminal [HO1]
+    (capture (cpers crim-pers) cb-cpc)     ; non-criminal captures criminal [cp already in use for crime propn]
+    (prevent (cb-cpc cb-vpp) cb-cpc->-vpp) ; non-criminal capturing criminal prevents criminal from victimizing non-criminal [HO1]
+    (danger-to (cpers) cb-dtp)             ; non-criminal is subject to danger
+    (cause (cb-cpc cb-dtp) cb-cpc->dtp)    ; non-criminal capturing criminal is dangerous to non-criminal [HO1]
    ))
 
 (defvar semantic-relations
