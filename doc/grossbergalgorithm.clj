7bf7b6f25ff0715e62c714230e149433bbbfc537
mars0i <mabrams@uab.edu>
2013-10-17 22:53:21 -0500

    text notes and clojure-code notes on Grossberg algorithm

--- doc/grossbergalgorithm.clj
+++ doc/grossbergalgorithm.clj
@@ -16,18 +16,29 @@
 (defn nonneg [x]
   (max 0 x))
 
-(def nonpos [x]
+(defn nonpos [x]
   (min 0 x))
 
-(defn clip-dist-from-min [activn]
-  (min 1 (+ 1 activn)))  ; think of sum as distance of neg activn from -1
+(defn dist-from-min [activn]
+  (+ 1 activn))  ; think of sum as distance of neg activn from -1
 
-(defn clip-dist-from-max [activn]
-  (max -1 (- 1 activn)))
+(defn dist-from-max [activn]
+  (- 1 activn))
+
+(defn clip-to-extrema [x]
+  (max -1 (min 1 x)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Data transformations
 ;; Steps should correspond to those listed in grossbergalgorithm.nts:
+;; References:
+;; "HT": Holyoak & Thagard 1989, "Analogue Retrieval by Constraint
+;; Satisfaction", Cognitive Science 13, pp. 295-355. See pp. 313, 315.
+;; "MR": Abrams 2013, "A Moderate Role for Cognitive Models in Agent-Based 
+;; Modeling of Cultural Change", Complex Adaptive Systems Modeling 2013.
+
+;; NEED TO TRIPLE CHECK EVERYTHING 
+;; against HT, MR, network.lisp, grossbergalgorithm.nts.
 
 ;; step 0
 (def activns 
@@ -37,21 +48,34 @@
                       #(repeatedly num-nodes rand-1+1))))
 
 ;; step 1
-(def pos-activns (emap nonneg activns)) ; neg activns will have no effect
+;; ; neg activns will have no effect
+(def pos-activns (emap nonneg activns)) ; max(0, a_j) in MR; o_i(t) in HT
 
 ;; step 2
-(def pos-wts (emap nonneg wts))
-(def neg-wts (emap nonpos wts))
+(def pos-wts (emap nonneg wts)) ; w_ij > 0 in MR
+(def neg-wts (emap nonpos wts)) ; w_ij < 0
 
 ;; step 3
-(def pos-wtd-inputs (mmul pos-wts pos-activns))
-(def neg-wtd-inputs (mmul neg-wts pos-activns))
+(def pos-wtd-inputs (mmul pos-wts pos-activns)) ; p_i in MR; enet_j in HT
+(def neg-wtd-inputs (mmul neg-wts pos-activns)) ; n_i in MR; inet_j in HT
 
 ;; step 4
-(def clipped-dists-from-min (emap clip-dist-from-min activns))
-(def clipped-dists-from-max (emap clip-dist-from-max activns))
+(def dists-from-max 
+  (emap dist-from-max activns)) ; .99 - a_i in MR; max - a_j(t) in HT
+(def dists-from-min 
+  (emap dist-from-min activns)) ; incorrect in MR; a_j(t) - min in HT
 
 ;; step 5
-(def decayed-activns (mul .9 activns))
+(def decayed-activns (mul 0.9 activns)) ; incorrect in MR; a_j(t)(1-d) in HT
 
 ;; step 6
+;; Almost final step
+(def unclipped-new-activns  ; s_i in MR; equation p. 313 in HT
+  (add decayed-activns 
+       (mul pos-wtd-inputs dists-from-max)
+       (mul neg-wtd-inputs dists-from-min)))
+
+;; step 7
+;; Now just pull back activns that have exceeded -1 and 1:
+(def new-activns 
+  (emap clip-to-extrema unclipped-new-activns)) ; a'_i in MR; p. 315 in HT
